9fff51cb6e6f55b0c4afeb87e92c9f4751fee402
Fer <Fmeji@hotmail.es>
2023-10-16 13:25:42 +0200

    Clase Main terminada y trabajo terminado

--- src/main/java/FactoryPrincipal.java
+++ src/main/java/FactoryPrincipal.java
@@ -3,7 +3,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class FactoryPrincipal {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int numBalls = 100000;
         int numSlots = 30;
 
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        FactoryPrincipal factoryPrincipal = new FactoryPrincipal();
+        factoryPrincipal.main(args);
+    }
+}
